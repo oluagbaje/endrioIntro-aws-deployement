db6570b9be1f778c2e6e9b5fe089290f19fcaa04
lenovo <lenovo@DESKTOP-6UAO80D>
2023-05-05 17:45:48 +0100

    Initial Commit

---
+++ src/main/java/com/endriosoft/endrio/app/controller/PersonController.java
@@ -0,0 +1,14 @@
+package com.endriosoft.endrio.app.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PersonController {
+	
+	@RequestMapping("/getname")
+	public String getName() {
+		return"endriosoft";
+	}
+
+}
